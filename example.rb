2801491e7b2a98581f88ba0bf9539f69c1d384c4
DESKTOP-VJVPAQ8\itoka <itokaryk@gmail.com>
2019-05-14 21:10:28 +0300

    tasts 1,2,3,4

--- example.rb
+++ example.rb
@@ -1,3 +1,50 @@
-10.times do 
-	print 'x'
+#LESSON 1
+#task 1
+text_1 = 'Hello'
+text_2 = 'World'
+
+puts text_1 + ' ' + text_2
+
+#task 2
+
+var = 'hello'
+
+var.each_char {|c| puts c}
+
+#task 3
+def check_if_greater(a)
+
+  result = false;
+  
+  if(a > 0 && a < 5)
+  	result = true
+  end
+
+  return result
+
 end
+
+puts check_if_greater(5);
+
+#task 4
+def day_part(min)
+
+  result = ''
+
+  case min
+  when 0..14
+    result = '1-st part'
+  when 15..29
+    result = '2-nd part'
+  when 30..44
+    result = '3-st part'
+  when 45..59
+  	result = '4-st part'
+  end
+
+  return result
+
+end
+
+puts day_part(36)
+puts day_part(45)
