b4356cf492a172f319a4d294992b86b150457245
ihortok <itokaryk@gmail.com>
2019-05-26 19:38:29 +0300

    move func works

--- oop-elevator/elevator.rb
+++ oop-elevator/elevator.rb
@@ -5,6 +5,7 @@ class Elevator
   attr_accessor :passengers
   attr_accessor :engines
   attr_accessor :routes
+  attr_accessor :direction
   attr_accessor :total_weight
 
   def initialize(floar)
@@ -18,6 +19,8 @@ class Elevator
   def add_passenger(floar, weight)
     @passengers << Passenger.new(floar, weight);
     @total_weight += weight
+    @routes << floar unless @routes.include?(floar)
+    @routes.sort!
   end
 
   def free_passenger()
@@ -29,23 +32,46 @@ class Elevator
     end
   end
 
+  def set_direction()
+    if @routes[0] > @floar
+      @direction = 'up'
+    else
+      @direction = 'down'
+    end
+  end
+
+  def move()
+    while @passengers.length > 0
+      if @floar == @routes[0]
+        self.free_passenger()
+        @routes.shift()
+      elsif @floar < @routes[0]
+        @direction = 'up' unless @direction == 'up'
+        @floar += 1
+      elsif @floar > @routes[0]
+      	@direction = 'down' unless @direction == 'down'
+      	@floar -= 1
+      end
+    end
+  end
 end
 
-elevator = Elevator.new(2)
+elevator = Elevator.new(1)
 
 elevator.add_passenger(2, 23)
 elevator.add_passenger(4, 13)
 elevator.add_passenger(6, 43)
 elevator.add_passenger(2, 43)
+elevator.add_passenger(3, 43)
 
+elevator.move
 
-
+puts elevator.floar
 
 #puts elevator.floar
 #puts elevator.passengers[0].weight
-puts elevator.passengers.length
-puts elevator.total_weight
-elevator.free_passenger
-puts elevator.passengers.length
-puts elevator.total_weight
-
+# puts elevator.passengers.length
+# puts elevator.total_weight
+# elevator.free_passenger
+# puts elevator.passengers.length
+# puts elevator.total_weight
