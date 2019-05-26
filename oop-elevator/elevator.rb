b348e26ef9ae3ce4fb5f38821b0ec8524d5283cc
ihortok <itokaryk@gmail.com>
2019-05-27 00:08:57 +0300

    some functional

--- oop-elevator/elevator.rb
+++ oop-elevator/elevator.rb
@@ -1,35 +1,62 @@
 load 'passenger.rb'
 
+module NewPassenger
+  
+end
+
 class Elevator
   attr_accessor :floar
   attr_accessor :passengers
+  attr_accessor :passengers_on_board
   attr_accessor :engines
   attr_accessor :routes
   attr_accessor :direction
   attr_accessor :total_weight
+  attr_accessor :log
 
   def initialize(floar)
     @floar = floar
     @passengers = []
+    @passengers_on_board = []
     @engines = []
     @routes = []
+    @log = []
     @total_weight = 0
   end
 
-  def add_passenger(floar, weight)
-    @passengers << Passenger.new(floar, weight);
-    @total_weight += weight
-    @routes << floar unless @routes.include?(floar)
-    @routes.sort!
+  def add_passenger(from, to, weight)
+    @passengers << Passenger.new(from, to, weight);
+    @routes << from #unless @routes.include?(from)
+    @routes << to #unless @routes.include?(to)
+  end
+
+  def enter_passenger()
+    counter = 0
+    indexes = []
+    @passengers.each_index do |i|
+      if @passengers[i].from == @floar
+      	counter += 1
+       	@passengers_on_board << @passengers[i]
+        @total_weight += @passengers[i].weight
+        indexes << i
+      end
+    end
+    indexes.each { |i| @passengers.delete_at(i) }
+    @log << "#{counter} passengers entered the elevator" unless counter.zero?
   end
 
   def free_passenger()
-    @passengers.each_index do |i|
-      if @passengers[i].floar == @floar
-        @total_weight -= @passengers[i].weight
-        @passengers.delete_at(i)
+  	counter = 0
+  	indexes = []
+    @passengers_on_board.each_index do |i|
+      if @passengers_on_board[i].to == @floar
+      	counter += 1
+        @total_weight -= @passengers_on_board[i].weight
+        indexes << i
       end
     end
+    indexes.each { |i| @passengers_on_board.delete_at(i) }
+    @log << "#{counter} passengers left the elevator" unless counter.zero?
   end
 
   def set_direction()
@@ -41,35 +68,51 @@ class Elevator
   end
 
   def move()
-    while @passengers.length > 0
-      if @floar == @routes[0]
+    # while @passengers.length > 0 || @passengers_on_board.length > 0
+    while @routes.length > 0
+      @log << "elevator is at the #{floar}-st floar"
+      if @routes.include?(@floar)
         self.free_passenger()
-        @routes.shift()
+        self.enter_passenger()
+        @routes.delete_at(@routes.index(@floar))
+        if @direction == 'up'
+          @floar += 1
+        else
+          @floar -= 1
+        end
       elsif @floar < @routes[0]
         @direction = 'up' unless @direction == 'up'
         @floar += 1
       elsif @floar > @routes[0]
-      	@direction = 'down' unless @direction == 'down'
-      	@floar -= 1
+        @direction = 'down' unless @direction == 'down'
+        @floar -= 1
       end
+      @log << "elevator goes #{direction}"
+      @log << "---"
     end
   end
 end
 
-elevator = Elevator.new(1)
+elevator = Elevator.new(7)
 
-elevator.add_passenger(2, 23)
-elevator.add_passenger(4, 13)
-elevator.add_passenger(6, 43)
-elevator.add_passenger(2, 43)
-elevator.add_passenger(3, 43)
+elevator.add_passenger(2, 5, 23)
+elevator.add_passenger(4, 9, 13)
+elevator.add_passenger(4, 9, 13)
+elevator.add_passenger(6, 15, 43)
+elevator.add_passenger(2, 19, 43)
+elevator.add_passenger(3, 12, 43)
+
+puts elevator.routes
 
 elevator.move
 
-puts elevator.floar
+puts elevator.log
 
-#puts elevator.floar
-#puts elevator.passengers[0].weight
+
+# puts elevator.routes
+
+# puts elevator.floar
+# puts elevator.passengers[0].weight
 # puts elevator.passengers.length
 # puts elevator.total_weight
 # elevator.free_passenger
--- oop-elevator/passenger.rb
+++ oop-elevator/passenger.rb
@@ -1,9 +1,19 @@
 class Passenger
-  attr_accessor :floar
+  attr_accessor :from
+  attr_accessor :to
   attr_accessor :weight
 
-  def initialize(floar, weight)
-    @floar = floar
+  def initialize(from, to, weight)
+    @from = from
+    @to = to
     @weight = weight
   end
 end
+
+# module NewPassenger
+#   attr_accessor :passengers_list
+
+#   def add_passenger(from, to, weight)
+#     @passengers_list << Passenger.new(from, to, weight);
+#   end
+# end
