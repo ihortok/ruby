1141b6e23518413e1a494b883da6400d32e47abf
ihortok <itokaryk@gmail.com>
2019-05-27 09:31:04 +0300

    move def fixed

--- oop-elevator/elevator.rb
+++ oop-elevator/elevator.rb
@@ -19,15 +19,15 @@ class Elevator
     @passengers = []
     @passengers_on_board = []
     @engines = []
-    @routes = []
+    @routes = {from: [], to: []}
     @log = []
     @total_weight = 0
   end
 
   def add_passenger(from, to, weight)
     @passengers << Passenger.new(from, to, weight);
-    @routes << from #unless @routes.include?(from)
-    @routes << to #unless @routes.include?(to)
+    @routes[:from] << from unless @routes[:from].include?(from)
+    @routes[:to] << to unless @routes[:to].include?(to)
   end
 
   def enter_passenger()
@@ -38,29 +38,35 @@ class Elevator
       	counter += 1
        	@passengers_on_board << @passengers[i]
         @total_weight += @passengers[i].weight
-        indexes << i
+        indexes << @passengers[i]
       end
     end
-    indexes.each { |i| @passengers.delete_at(i) }
-    @log << "#{counter} passengers entered the elevator" unless counter.zero?
+    unless counter.zero?
+      indexes.each { |i| @passengers.delete(i) }
+      @routes[:from].delete(@floar)
+      @log << "#{counter} passengers entered the elevator"
+    end
   end
 
   def free_passenger()
-  	counter = 0
-  	indexes = []
+    counter = 0
+    indexes = []
     @passengers_on_board.each_index do |i|
       if @passengers_on_board[i].to == @floar
-      	counter += 1
+        counter += 1
         @total_weight -= @passengers_on_board[i].weight
-        indexes << i
+        indexes << @passengers_on_board[i]
       end
     end
-    indexes.each { |i| @passengers_on_board.delete_at(i) }
-    @log << "#{counter} passengers left the elevator" unless counter.zero?
+    unless counter.zero?
+      indexes.each { |i| @passengers_on_board.delete(i) }
+      @routes[:to].delete(@floar)
+      @log << "#{counter} passengers left the elevator"
+    end
   end
 
   def set_direction()
-    if @routes[0] > @floar
+    if @routes[:to][0] > @floar
       @direction = 'up'
     else
       @direction = 'down'
@@ -69,26 +75,25 @@ class Elevator
 
   def move()
     # while @passengers.length > 0 || @passengers_on_board.length > 0
-    while @routes.length > 0
+    while @routes[:from].length > 0 || @routes[:to].length > 0
       @log << "elevator is at the #{floar}-st floar"
-      if @routes.include?(@floar)
+      # @passengers_on_board.each { |e| @log << e.to }
+      if @routes[:from].include?(@floar) || @routes[:to].include?(@floar)
         self.free_passenger()
         self.enter_passenger()
-        @routes.delete_at(@routes.index(@floar))
         if @direction == 'up'
           @floar += 1
         else
           @floar -= 1
         end
-      elsif @floar < @routes[0]
+      elsif @floar < @routes[:to][0]
         @direction = 'up' unless @direction == 'up'
         @floar += 1
-      elsif @floar > @routes[0]
+      elsif @floar > @routes[:to][0]
         @direction = 'down' unless @direction == 'down'
         @floar -= 1
       end
       @log << "elevator goes #{direction}"
-      @log << "---"
     end
   end
 end
@@ -99,15 +104,15 @@ elevator.add_passenger(2, 5, 23)
 elevator.add_passenger(4, 9, 13)
 elevator.add_passenger(4, 9, 13)
 elevator.add_passenger(6, 15, 43)
+elevator.add_passenger(6, 15, 43)
 elevator.add_passenger(2, 19, 43)
 elevator.add_passenger(3, 12, 43)
 
-puts elevator.routes
+#puts elevator.routes
 
 elevator.move
 
 puts elevator.log
-
 
 # puts elevator.routes
 
